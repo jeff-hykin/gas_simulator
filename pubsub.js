a5f678405f3ec98f84589fb985e3f6f8413c8777
Jeff Hykin <jeff.hykin@gmail.com>
2026-02-05 14:24:59 -0800

    add computed

--- pubsub.js
+++ pubsub.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env -S deno run --allow-all
+
 /**
  * Create a publish/subscribe message bus factory with publisher identity tracking.
  * Returns a function that requires an identity and returns a pubsub object.
@@ -15,7 +17,7 @@
 export function createPubSub() {
     const subs = {}
 
-    return function(who) {
+    return function pubsubFactory(who) {
         if (!who) throw new Error("pubsub requires an identity (who parameter)")
 
         /**
@@ -46,12 +48,68 @@ export function createPubSub() {
             if (!list) return
             for (const entry of list) {
                 // Only call callback if publisher is different from subscriber
-                if (entry.subscriber !== who) {
+                // if (entry.subscriber !== who) {
                     entry.callback(data, who)
-                }
+                // }
             }
         }
 
-        return { subscribe, publish, who }
+        return { subscribe, publish, who, new: pubsubFactory }
+    }
+}
+
+export class Computed {
+    constructor({pubsub, initValue, topics, name}, callback) {
+        this.pubsub = pubsub.new(`computed-${name}`)
+        this.value = initValue
+        const activeArg = {}
+        this.subscribers = []
+        this.unsubs = []
+        const wrappedCallback = (...args)=>{
+            this.value = callback(activeArg)
+            for (let each of this.subscribers) {
+                try {
+                    this.value = each(this.value)
+                } catch (error) {
+                    console.warn(error.stack||error)
+                }
+            }
+        }
+        for (const [key, value] of Object.entries(topics)) {
+            if (value instanceof Computed) {
+                const computed = value
+                this.unsubs.push(
+                    value.subscribe((newValue)=>{
+                        activeArg[key] = newValue
+                        wrappedCallback(activeArg, key, computed)
+                    })
+                )
+            } else {
+                this.unsubs.push(
+                    pubsub.subscribe(key, (value, who)=>{
+                        console.log(`received ${value} from ${who}`)
+                        activeArg[key] = value
+                        wrappedCallback(activeArg, key, who)
+                    })
+                )
+            }
+        }
+    }
+    unsub() {
+        for (const each of this.unsubs) {
+            try {
+                eachUnsub()
+            } catch (error) {
+                console.warn(error.stack||error)
+            }
+        }
+        this.unsubs.length = 0
+    }
+    subscribe(callback) {
+        this.subscribers.push(callback)
+        return ()=>{
+            const index = this.subscribers.indexOf(callback)
+            if (index !== -1) this.subscribers.splice(index, 1)
+        }
     }
 }
